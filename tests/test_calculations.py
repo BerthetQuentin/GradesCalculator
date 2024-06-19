5d5e7201e6e0ba15e4841d94e40d0a6259490007
Quentin Berthet <quentin@berthet.ch>
2024-06-19 17:23:40 +0200

    Create test_calculations.py

---
+++ tests/test_calculations.py
@@ -0,0 +1,17 @@
+# tests/test_calculations.py
+
+from gradescalculator.main import calculate_grades
+
+def test_calculate_grades():
+    grades = {
+        "grade 1": {"weight": 10, "grade": 3.6},
+        "grade 2": {"weight": 15, "grade": 4.6},
+        "grade 3": {"weight": 25, "grade": 5.3},
+        "grade 4": {"weight": 5, "grade": 4.0},
+        "grade 5": {"weight": 45, "grade": 4.9}
+    }
+    
+    precise_grade, rounded_grade = calculate_grades(grades)
+    
+    assert precise_grade == 4.7  # Mettre à jour avec la valeur attendue
+    assert rounded_grade == 4.5  # Mettre à jour avec la valeur attendue
