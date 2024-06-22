426c6d6413f9a8e0c97918e95d6ea1ccbdb7f3c7
Quentin Berthet <123391834+BERTHETquentin@users.noreply.github.com>
2024-06-22 11:57:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gradescalculator',
-    version='0.5',
+    version='1.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
