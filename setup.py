be088649c411f9d0921c7424627fefd5bb6c69c8
Quentin Berthet <quentin@berthet.ch>
2024-10-03 11:28:10 +0200

    rename folder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gradescalculator',
-    version='2.0.1',
+    version='2.0.2',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
